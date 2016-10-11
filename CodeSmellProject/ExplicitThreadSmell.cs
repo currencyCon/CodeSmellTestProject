42bbc13eaab4c347f2f5540ebad4da798b0716cd
Markus Schaden <mrksschdn@gmail.com>
2016-10-11 09:43:32 +0200

    Renamed project

--- CodeSmellProject/ExplicitThreadSmell.cs
+++ CodeSmellProject/ExplicitThreadSmell.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace CodeSmellTestProject
+namespace CodeSmellProject
 {
     class ExplicitThreadSmell
     {
--- CodeSmellProject/Program.cs
+++ CodeSmellProject/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CodeSmellTestProject
+namespace CodeSmellProject
 {
     class Program
     {
